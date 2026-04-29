829c89ed91deb2de19e67e5008f9ebab40502b20
Rodolphe Latour <rodolphe.latour@gmail.com>
2026-04-29 17:51:28 +0200

    security(biography): New pydantic schema for biography update
    - Schema validation for update

---
+++ backend/Schemas/biography.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class ImageSizeIn(BaseModel):
+    sm: str
+    md: str
+    lg: str
+
+    model_config = {
+        "extra": "forbid",
+    }
+
+
+class SectionIn(BaseModel):
+    title: str
+    paragraphs: List[str]
+    model_config = {
+        "extra": "forbid",
+    }
+
+
+class BiographyIn(BaseModel):
+    id: str = Field(alias="_id")
+    title: str
+    image: ImageSizeIn
+    sections: List[SectionIn]
+
+    model_config = {
+        "extra": "forbid",
+        "populate_by_name": True
+    }
--- backend/routes/biography.py
+++ backend/routes/biography.py
@@ -1,7 +1,10 @@
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required
+from mongoengine import DoesNotExist, ValidationError
+from pydantic import ValidationError as PydanticValidationError
+from Schemas.biography import BiographyIn
 from helpers import handle_db_timeout
-from models.biography import Biography
+from models.biography import Biography, ImageSize, Section
 from datetime import datetime, timezone
 
 biography_bp = Blueprint('biography', __name__)
@@ -13,15 +16,26 @@ def get_biography():
     biography = Biography.objects.first()
     return jsonify({"biography": biography.to_json_dict()})
 
+
 @biography_bp.route('/biography', methods=['PUT'])
 @jwt_required()
 @handle_db_timeout
 def update_biography():
-    data = request.get_json()
-    data.pop('_id', None)
-    data['updatedAt'] = datetime.now(timezone.utc)
-    update_data = {f"set__{key}": value for key, value in data.items()}
-    if not update_data:
-        return jsonify({'error': 'No data to update'}), 400
-    Biography.objects.update_one(**update_data)
-    return jsonify({'updated': True}), 200
+    if not request.is_json:
+        return jsonify({"error": "invalid content-type"}), 415
+    try:
+        data = BiographyIn.model_validate(request.get_json())
+    except PydanticValidationError as e:
+        return jsonify({"error": "invalid payload"}), 400
+    try:
+        bio = Biography.objects.get(id=data.id)
+        bio.title = data.title
+        bio.image = ImageSize(**data.image.model_dump())
+        bio.sections = [Section(**s.model_dump()) for s in data.sections]
+        bio.updatedAt = datetime.now(timezone.utc)
+        bio.save()
+        return jsonify({"updated": True}), 200
+    except DoesNotExist:
+        return jsonify({"error": "biography not found"}), 404
+    except ValidationError:
+        return jsonify({"error": "invalid ID"}), 400
