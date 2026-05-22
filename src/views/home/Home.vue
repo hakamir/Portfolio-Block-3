a1828ded0b122a1c975209b33112fc0d6b5f8273
Rodolphe Latour <rodolphe.latour@gmail.com>
2026-05-22 16:05:57 +0200

    feat(accessibility): Add aria-labels in home page and navbar
    - style correction of gradient mask on hero page

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -7,10 +7,10 @@ import Footer from "@components/layout/footer/Footer.vue";
 <template>
   <Hero></Hero>
   <Biography></Biography>
-  <section class="relative min-h-screen bg-neutral-300 snap-start flex flex-col">
+  <section class="relative min-h-screen bg-neutral-300 snap-start flex flex-col" aria-labelledby="contact-title">
     <div class="container mx-auto px-4 pt-32 pb-20 flex-1 flex items-center">
       <div>
-        <h2 class="text-3xl font-bold mb-4 font-unbounded">Contact Me</h2>
+        <h2 id="contact-title" class="text-3xl font-bold mb-4 font-unbounded">Contact Me</h2>
         <p class="text-lg pb-4">Ready to collaborate? Let's discuss your project and how I can contribute.</p>
         <RouterLink to="/contact"
                     class="bg-black text-white px-6 py-3 font-unbounded rounded transition-all duration-300 hover:bg-neutral-700 inline-block">
